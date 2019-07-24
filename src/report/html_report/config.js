597688189c60bf206f0e408e242608ae6e7f0c4f
vladyslav223 <vladyslav223@gmail.com>
2019-07-24 21:00:25 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,60 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_entire-document_0_document_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190724-195806\\test_entire-document_0_document_0_tablet_h.png",
+        "selector": "document",
+        "fileName": "test_entire-document_0_document_0_tablet_h.png",
+        "label": "entire-document",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/style-it-up/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found E:\\Програмы\\Git\\layout_style-it-up\\backstop_data\\bitmaps_reference\\test_entire-document_0_document_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_default-block_0_qa-block_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190724-195806\\test_default-block_0_qa-block_0_tablet_h.png",
+        "selector": "#qa-block",
+        "fileName": "test_default-block_0_qa-block_0_tablet_h.png",
+        "label": "default-block",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/style-it-up/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found E:\\Програмы\\Git\\layout_style-it-up\\backstop_data\\bitmaps_reference\\test_default-block_0_qa-block_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_hovered-block_0_qa-block_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190724-195806\\test_hovered-block_0_qa-block_0_tablet_h.png",
+        "selector": "#qa-block",
+        "fileName": "test_hovered-block_0_qa-block_0_tablet_h.png",
+        "label": "hovered-block",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/style-it-up/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "error": "Reference file not found E:\\Програмы\\Git\\layout_style-it-up\\backstop_data\\bitmaps_reference\\test_hovered-block_0_qa-block_0_tablet_h.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "test"
+});
